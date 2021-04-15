8e01d93e9daa4d759602a74c115dfea60494b6cd
FrankHausmann <fhausmann@unicomedv.de>
2021-04-15 18:44:33 +0200

    Kiss changes

---
+++ KissXML/KissXML.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+#if __has_include("DDXMLElementAdditions.h")
+    #import "DDXMLElementAdditions.h"
+#endif
+#import "NSString+DDXML.h"
+#import "DDXML.h"
+#import "DDXMLDocument.h"
+#import "DDXMLElement.h"
+#import "DDXMLNode.h"
+
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
